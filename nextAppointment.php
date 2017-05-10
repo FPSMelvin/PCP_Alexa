302e13b988642d37b140a1150f44d67883b7bb11
Melvin Sterk <git@melvinsterk.nl>
2017-05-10 11:40:53 +0200

    updated to calculate difference between appointment

--- nextAppointment.php
+++ nextAppointment.php
@@ -1,5 +1,15 @@
 <?php
 function nextAppointment(){
-    $ssml = "<speak>Your next appointment is in four hours</speak>";
+    //Get moment untill next appointment
+    $time = time();
+    $appointmentTime = strtotime("15:30:00 GMT+1");
+    $timeDifference = $appointmentTime - $time;
+
+    //Calculate time difference in hours and seconds
+    $hours = floor($timeDifference / 3600);
+    $mins = floor($timeDifference / 60 % 60);
+
+
+    $ssml = "<speak>Your next appointment is in " . $hours . " hours and " . $mins . " minutes.</speak>";
     return $ssml;
 }
