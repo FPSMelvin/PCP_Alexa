822262909f02daeb437d7594785e0cc4ad1d4b1f
TawabG <tghorbandi@gmail.com>
2017-05-10 15:57:40 +0200

    launch request

--- alexa.php
+++ alexa.php
@@ -8,13 +8,14 @@ $json = json_decode($data, true);
 if(isset($json['session']['application']['applicationId'])){
 
     $Id     = $json['session']['application']['applicationId'];
-    //$type   = $json['request']['type'];
+    $type   = $json['request']['type'];
     $name   = $json['request']['intent']['name'];
-    //$day = $json['request']['intent']['slots']['day']['value'];
 
     $ssml;
 
-    //$ssml   = errorMessage();
+    if ($type == "LaunchRequest"){
+      $ssml = launchRequest();
+    }
 
     switch ($name) {
         case "NextAppointment":
