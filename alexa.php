2d5cc898d65fed0183961774c2f77a57958dcefa
TawabG <tghorbandi@gmail.com>
2017-05-12 15:21:56 +0200

    ..

--- alexa.php
+++ alexa.php
@@ -6,26 +6,60 @@ $data = json_decode( file_get_contents('php://input') );
 
 
 if (isset($data) && isset($data->request) && isset($data->request->dialogState) && $data->request->dialogState == 'COMPLETED') {
+
+  $day = $data->request->intent->slots->day->value;
+  $ssml = dailySchedule($day);
+
+  $array = array(
+      "version" => "1.0",
+      "sessionAttributes" => array(),
+      "reponse" => array(
+          "outputSpeech" => array(
+              "type" => "SSML",
+              "ssml" => $ssml
+              )
+      ),
+      "card" => array(
+          "type" => "Simple",
+          "title" => "SessionSpeechlet - Travel booking",
+          "content" => "SessionSpeechlet - This will be fun. hiking from eindhoven to amsterdam on 2017-04-21"
+      ),
+      "reprompt" => array(
+          "outputSpeech" => array(
+              "type" => "PlainText",
+              "text" => ""
+          )
+      ),
+      "shouldEndSession" => true
+  );
+
+  $response = $array;
+
 }
 else{
-    $dialogDelegate = json_decode('{
-    "version": "1.0",
-    "sessionAttributes": {},
-    "response": {
-        "outputSpeech": null,
-        "card": null,
-        "directives": [
-            {
-                "type": "Dialog.Delegate"
-            }
-        ],
-        "reprompt": null,
-        "shouldEndSession": false
-    }
-}');
-    $response = $dialogDelegate;
+  $delegate = '{
+      "version": "1.0",
+      "sessionAttributes": {},
+      "response": {
+          "outputSpeech": null,
+          "card": null,
+          "directives": [
+              {
+                  "type": "Dialog.Delegate"
+              }
+          ],
+          "reprompt": null,
+          "shouldEndSession": false
+      }
+  }';
+  $response = $delegate;
 }
+
+
+
+
+
+
 echo json_encode($response);
 
 ?>
-
