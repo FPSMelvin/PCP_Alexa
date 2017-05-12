e28d89b35e39d30bab612df62281513034992166
TawabG <tghorbandi@gmail.com>
2017-05-12 16:03:46 +0200

    ..

--- alexa.php
+++ alexa.php
@@ -3,33 +3,34 @@ require 'functions.php';
 header('Content-Type: application/json');
 
 $data = json_decode( file_get_contents('php://input') );
-$ssml = "<speak>melvin is a bad programmer</speak>";
+$ssml = "<speak>testing</speak>";
 
-// $ssml = "<speak>Empty</speak>";
-// if(isset($data['request']['intent']['name'])){
-//     $name   = $data['request']['intent']['name'];
-//
-//     switch ($name) {
-//        case "NextAppointment":
-//            $ssml = nextAppointment();
-//            break;
-//        case "DailyScheduleIntent":
-//           if (isset($json['request']['intent']['slots']['day']['value'])) {
-//              $day = $json['request']['intent']['slots']['day']['value'];
-//              $ssml = dailySchedule($day);
-//            }
-//            break;
-//        case "testIntent":
-//            $ssml = testGeluid();
-//            break;
-//        default:
-//            $ssml = "<speak>Empty</speak>";
-//
-//     }
-// }
 
 
 if (isset($data) && isset($data->request) && isset($data->request->dialogState) && $data->request->dialogState == 'COMPLETED') {
+
+
+  // if(isset($data['request']['intent']['name'])){
+  //     $name   = $data['request']['intent']['name'];
+  //
+  //     switch ($name) {
+  //        case "NextAppointment":
+  //            $ssml = nextAppointment();
+  //            break;
+  //        case "DailyScheduleIntent":
+  //           if (isset($json['request']['intent']['slots']['day']['value'])) {
+  //              $day = $json['request']['intent']['slots']['day']['value'];
+  //              $ssml = dailySchedule($day);
+  //            }
+  //            break;
+  //        case "testIntent":
+  //            $ssml = testGeluid();
+  //            break;
+  //        default:
+  //            $ssml = "<speak>Empty</speak>";
+  //
+  //     }
+  // }
 
 ?>
   {
@@ -55,39 +56,9 @@ if (isset($data) && isset($data->request) && isset($data->request->dialogState) 
       }
   }
   <?php
-
-  // $day = $data->request->intent->slots->day->value;
-  // $ssml = dailySchedule($day);
-  //
-  // $array = array(
-  //     "version" => "1.0",
-  //     "sessionAttributes" => array(),
-  //     "reponse" => array(
-  //         "outputSpeech" => array(
-  //             "type" => "SSML",
-  //             "ssml" => $ssml
-  //             )
-  //     ),
-  //     "card" => array(
-  //         "type" => "Simple",
-  //         "title" => "SessionSpeechlet - Travel booking",
-  //         "content" => "SessionSpeechlet - This will be fun. hiking from eindhoven to amsterdam on 2017-04-21"
-  //     ),
-  //     "reprompt" => array(
-  //         "outputSpeech" => array(
-  //             "type" => "PlainText",
-  //             "text" => ""
-  //         )
-  //     ),
-  //     "shouldEndSession" => true
-  // );
-  //
-  // $response = $array;
-  //
-  // echo json_encode($response);
-
 }
 else{
+
   ?>
   {
       "version": "1.0",
