e03c3c4d9d3e0208f3a16202ccb917d30bb705b4
Melvin Sterk <git@melvinsterk.nl>
2017-05-12 15:04:35 +0200

    test

--- alexa.php
+++ alexa.php
@@ -1,159 +1,30 @@
 <?php
-
-require 'nextAppointment.php';
-
-$data = file_get_contents('php://input');
-$json = json_decode($data, true);
-
-$day;
-$ssml;
-$delegate = false;
-
-//file_put_contents('test4.txt', print_r($json, true).PHP_EOL.'-----'.PHP_EOL, FILE_APPEND);
-//var_dump($json['request']['type']);
-
-if(isset($json['request']['type'])){
-   $type = $json['request']['type'];
-   if ($type == "LaunchRequest"){
-       $ssml = launchRequest();
-   }
-   //var_dump($ssml);
-}
-
-
-
-// check if response has a session and applicationid
-if(isset($json['session']['application']['applicationId'])){
-
-   $Id     = $json['session']['application']['applicationId'];
-   $name   = $json['request']['intent']['name'];
-
-   switch ($name) {
-       case "NextAppointment":
-           $ssml = nextAppointment();
-           break;
-       case "DailyScheduleIntent":
-          if (isset($json['request']['intent']['slots']['day']['value'])) {
-             $day = $json['request']['intent']['slots']['day']['value'];
-             $ssml = dailySchedule($day);
-           }else{
-             $delegate = true;
-             //$ssml = errorMessage();
-           }
-           break;
-       case "testIntent":
-           $ssml = testGeluid();
-           break;
-       default:
-           $array = array(
-               "response" => array(
-                   "outputSpeech" => array(
-                       "type" => "SSML",
-                       "ssml" => $ssml
-                       )
-               )
-           );
-
-           $response = $array;
-   }
-
-   // if($type == "LaunchRequest"){
-   //   $ssml = launchRequest();
-   // }else{
-   //   switch ($name) {
-   //       case "NextAppointment":
-   //           $ssml = nextAppointment();
-   //           break;
-   //       case "DailyScheduleIntent":
-   //           $ssml = dailySchedule($day);
-   //           break;
-   //       case "":
-   //           echo "";
-   //           break;
-   //       default:
-   //         $array = array(
-   //             "response" => array(
-   //                 "outputSpeech" => array(
-   //                     "type" => "SSML",
-   //                     "ssml" => $ssml
-   //                     )
-   //             )
-   //         );
-   //
-   //         $response = $array;
-   //   }
-   // }
-
-   $array = array(
-       "response" => array(
-           "outputSpeech" => array(
-               "type" => "SSML",
-               "ssml" => $ssml
-               )
-       )
-   );
-
-   $response = $array;
-
-}
-
-$dialogDelegate = array(
-   "type" => "Dialog.Delegate",
-);
-
-
-
-$array = array(
-   "response" => array(
-       "outputSpeech" => array(
-           "type" => "SSML",
-           "ssml" => $ssml
-       )
-   )
-);
-$response = $array;
-
-
-if ($delegate == true){
-  $response = $dialogDelegate;
-  $delegate = false;
-}
-
-//Check if it should delegate or send out outputSpeech
-//$delegate ? $response = $dialogDelegate : $response = $array;
-
-
-
-// $json = json_decode('{
-//    "version":"1.0",
-//    "shouldEndSession":"false",
-//    "response":{
-//       "directives":[
-//          {
-//             "type":"Dialog.Delegate",
-//             "updatedIntent":{
-//                "name":"DailyScheduleIntent",
-//                "confirmationStatus":"NONE",
-//                "slots":{
-//                   "day":{
-//                      "name":"day",
-//                      "confirmationStatus":"NONE"
-//                   }
-//                }
-//             }
-//          }
-//       ]
-//    }
-// }');
-//
-//
-// $response = array("response" => $json);
-// $response = $json;
-
-
 header('Content-Type: application/json');
+
+$data = json_decode( file_get_contents('php://input') );
+
+
+if (isset($data) && isset($data->request) && isset($data->request->dialogState) && $data->request->dialogState == 'COMPLETED') {
+}
+else{
+    $dialogDelegate = json_decode('{
+    "version": "1.0",
+    "sessionAttributes": {},
+    "response": {
+        "outputSpeech": null,
+        "card": null,
+        "directives": [
+            {
+                "type": "Dialog.Delegate"
+            }
+        ],
+        "reprompt": null,
+        "shouldEndSession": false
+    }
+}');
+    $response = $dialogDelegate;
+}
 echo json_encode($response);
 
-//file_put_contents('test3.txt', json_encode($response).PHP_EOL, FILE_APPEND);
-
 ?>
+
